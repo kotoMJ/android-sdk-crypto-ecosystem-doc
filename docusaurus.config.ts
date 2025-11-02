7bde2a0be4237f13168744d6640049c698074cf4
Michal Jenicek <michal.jenicek@strv.com>
2025-11-02 19:58:28 +0100

    Customize default theme and navbar title color.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -142,8 +142,13 @@ const config: Config = {
     themeConfig: {
         // Replace with your project's social card
         image: "img/docusaurus-social-card.jpg",
+        colorMode: {
+            defaultMode: 'dark',
+            disableSwitch: false,
+            respectPrefersColorScheme: false,
+        },
         navbar: {
-            title: "",
+            title: "CRYPTO SDK",
             style: "dark",
             logo: {
                 alt: "CRYPTO Logo",
